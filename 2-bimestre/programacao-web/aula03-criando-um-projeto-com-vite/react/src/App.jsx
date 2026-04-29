63e57624bea1966aa75398d941a998a043651832
João Mateus <joao.sousa1601@aluno.ce.gov.br>
2026-04-29 17:19:59 +0000

    projeto feito

---
+++ 2-bimestre/programacao-web/aula03-criando-um-projeto-com-vite/react/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  const name = "Mateus"
+  return(
+    <div>
+      <h1>Meu Primeiro Projeto React com Vite</h1> 
+      <h1>Meu nome é {name}</h1>
+    </div>
+  )
+}
