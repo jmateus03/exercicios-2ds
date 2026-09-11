270263bbba8c58e9a8bc1b116a66e65e7550c856
João Mateus <joao.sousa1601@aluno.ce.gov.br>
2026-09-11 18:59:17 +0000

    ajeitando jest

--- 3-bimestre/orientacao-a-objetos/projetos/projeto3/Factory.js
+++ 3-bimestre/orientacao-a-objetos/projetos/projeto3/Factory.js
@@ -1,6 +1,18 @@
 const Notificacao = require("./Notificacao")
 // CRIE SUA SOLUÇÃO ABAIXO ================
-
+class factory {
+    static criar(tipo, mensagem){
+        if(tipo === "email"){
+            return new Email(mensagem)
+        }else if(tipo === "sms"){
+            return new SMS(mensagem)
+        }else if(tipo === "app"){
+            return new App(mensagem)
+        }else{
+            return "Tipo de notificação inválido"
+        }
+    }
+}
 
 
 
